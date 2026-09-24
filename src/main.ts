322acd1e83ce8cbcf27e97da874e11a8c6767a40
Daniel <stuermer.dan137@gmail.com>
2026-09-24 18:41:48 +0200

    added functionality to change image depending on chosen theme

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import './scss/main.scss'
+
+const themeRadios = document.querySelectorAll<HTMLInputElement>('input[name="theme"]');
+const themeImage = document.querySelector<HTMLImageElement>('.settings-box-right img');
+
+const themeImages: Record<string, string> = {
+    'Code vibes theme': '/assets/images/settings-screen/it-theme-logo.png',
+    'DA Projects theme': '/assets/images/settings-screen/da-theme-logo.png'
+};
+
+themeRadios.forEach((radio) => {
+    radio.addEventListener('change', () => {
+        const labelText = radio.parentElement?.querySelector('.label-text')?.textContent;
+        if (labelText && themeImages[labelText] && themeImage) {
+            themeImage.src = themeImages[labelText];
+        }
+    });
+});
