8c135594f41f03e52cfffc09fbe3ee73a126bfac
Daniel <stuermer.dan137@gmail.com>
2026-09-24 20:03:36 +0200

    added start-button functionality to be active after choosing 3 options and redirection to game.html

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,44 @@ themeRadios.forEach((radio) => {
         }
     });
 });
+
+const themeValueEl = document.querySelector<HTMLParagraphElement>('.theme-value');
+const playerValueEl = document.querySelector<HTMLParagraphElement>('.player-value');
+const boardValueEl = document.querySelector<HTMLParagraphElement>('.board-value');
+const startBtn = document.querySelector<HTMLButtonElement>('.start-btn');
+
+
+const selections = {
+    theme: '',
+    player: '',
+    board: '',
+};
+
+function checkAllSelected() {
+    if (selections.theme && selections.player && selections.board) {
+        startBtn!.disabled = false;
+    }
+}
+
+function bindRadioGroup(name: string, targetEl: HTMLParagraphElement | null, key: keyof typeof selections) {
+    const radios = document.querySelectorAll<HTMLInputElement>(`input[name="${name}"]`);
+
+    radios.forEach((radio) => {
+        radio.addEventListener('change', () => {
+            const labelText = radio.parentElement?.querySelector('.label-text')?.getAttribute('data-text');
+            if (labelText && targetEl) {
+                targetEl.textContent = labelText;
+                selections[key] = labelText;
+                checkAllSelected();
+            }
+        });
+    });
+}
+
+bindRadioGroup('theme', themeValueEl, 'theme');
+bindRadioGroup('player', playerValueEl, 'player');
+bindRadioGroup('board', boardValueEl, 'board');
+
+startBtn?.addEventListener('click', () => {
+    window.location.href = './game.html';
+});
